3aef886c47ccdf2dc603f260a2b9479a765fa9be
hey-Zayn <zaynobusiness@gmail.com>
2025-11-02 13:32:28 +0500

    Branch 5

--- frontend/src/axios/authInstance.js
+++ frontend/src/axios/authInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const authInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: 'https://job-application-mern-nu.vercel.app//api/v1',
     withCredentials: true,
 })
 
